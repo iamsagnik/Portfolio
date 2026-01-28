a52a4b0a58afdce923b72c9841a4ce70c97810a2
SAGNIK_MITRA <sagnikmitra008@gmail.com>
2026-01-28 12:24:55 +0530

    Reorganizing folder structure

--- src/hooks/index.js
+++
@@ -1,3 +0,0 @@
-import useCardLayout from "./useCardLayout";
-
-export { useCardLayout };
--- src/pages/blog/index.js
+++
@@ -1,8 +0,0 @@
-import { i } from "framer-motion/client";
-import BlogCard from "./BlogCard";
-import BlogPostgrid from "./BlogPostgrid";
-import Wall from "./Wall";
-import MonsterWatcher from "./MonsterWatcher";
-import SpeechCloud from "./SpeechCloud";
-
-export { BlogCard, BlogPostgrid, Wall, MonsterWatcher, SpeechCloud };
